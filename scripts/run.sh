b4ddc49170ad821a21ba4f4ed85e58652c78972c
段纪刚 <duanjigang@chalco-steering.com>
2018-04-24 20:51:11 +0800

    modify run scripts

--- scripts/run.sh
+++ scripts/run.sh
@@ -8,6 +8,10 @@ shift
 ## set config
 ### logs path
 logpath=/home/jigangduan/workspace/micro/logs
+if [ ! -d $logpath ] ; then
+  echo "$logpath : No such directory"
+  exit 1
+fi
 ### mongodb URL
 database_url=mongodb://root:GCCTS123@dds-bp18ebd1d16fc5b41515-pub.mongodb.rds.aliyuncs.com:3717,dds-bp18ebd1d16fc5b42738-pub.mongodb.rds.aliyuncs.com:3717/admin?replicaSet=mgset-5412007
 
@@ -23,6 +27,7 @@ stop_consul() {
 ### SRV
 start_srv() {
   srv=$1
+  shift
   case $srv in
 	  account)
     echo 'start account srv!'
@@ -37,15 +42,58 @@ start_srv() {
 	  shop-srv --database_url=${database_url} 1>>${logpath}/srv/info.log 2>>${logpath}/srv/error.log &
 	  ;;
 	  *)
-	  echo "$0 <account|jwtauth|shop>"
+	  echo "run.sh start $0 <account|jwtauth|shop>"
 	  exit
 	  ;;
   esac
 }
 
-### API
+### APIs
 start_api() {
-  echo 'start api cmd'
+  endpoint=$1
+  shift
+  case $endpoint in
+	  center)
+	  center_apis $*
+	  ;;
+	  merchant)
+	  merchant_apis $*
+	  ;;
+    applet)
+	  applet_apis $*
+	  ;;
+	  *)
+	  echo "run.sh start $0 <center|merchant|applet>"
+	  exit
+	  ;;
+  esac
+}
+
+center_apis() {
+  api=$1
+  shift
+  case $api in
+	  auth)
+    echo 'start center auth api!'
+	  center-api --register_ttl=30 --register_interval=15 auth --api_service=com.btdxcx.center.api.auth --site_type=center 1>>${logpath}/api/center/info.log 2>>${logpath}/api/center/error.log &
+	  ;;
+	  shop)
+    echo 'start center shop apis!'
+	  center-api --register_ttl=30 --register_interval=15 shop 1>>${logpath}/api/center/info.log 2>>${logpath}/api/center/error.log &
+	  ;;
+	  *)
+	  echo "run.sh start api $0 <auth|shop>"
+	  exit
+	  ;;
+  esac
+}
+
+merchant_apis() {
+  echo 'start merchant apis'
+}
+
+applet_apis() {
+  echo 'start applet apis'
 }
 
 start() {
@@ -56,10 +104,10 @@ start() {
 	  start_srv $*
 	  ;;
 	  api)
-	  start_api
+	  start_api $*
 	  ;;
 	  *)
-	  echo "$0 <srv|api>"
+	  echo "run.sh $0 <srv|api>"
 	  exit
 	  ;;
   esac
